69f5d583fdd60f15a7a6514413067e26f7fe336c
Robin Kaup <robin@robinkaup.com>
2012-07-10 16:59:25 +0100

    Add vim-fugitive to .vim/bundles.

--- .vim/bundle/Install.zsh
+++ .vim/bundle/Install.zsh
@@ -3,4 +3,5 @@
 git clone https://github.com/kien/rainbow_parentheses.vim.git
 git clone git://github.com/altercation/vim-colors-solarized.git
 git clone git://github.com/tpope/vim-surround.git
+git clone git://github.com/tpope/vim-fugitive.git
 
